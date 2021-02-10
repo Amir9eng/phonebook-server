e6f64a0320eb91e36987d19edfc6f4bdfd785869
Amir9eng <amirmukhtarme@gmail.com>
2021-02-10 17:21:36 +0100

    3.2: Phonebook backend step2

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.json());
 
-let contacts = [
+let persons = [
   {
     name: "Ada Lovelace",
     number: "39-44-5323523",
@@ -33,11 +33,30 @@ let contacts = [
 ];
 
 app.get("/api/persons", (request, response) => {
-  response.json(contacts);
+  response.json(persons);
 });
 
 app.get("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+  if (person) {
+    response.json(person);
+  }
+});
+
+app.get("/info", (request, response) => {
+  const date = new Date().toLocaleString();
+  const created = new Date(Date.now());
+  const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+  response.send(
+    ` <div>
+          <p>Phonebook has info for ${persons.length} people</p>
+      
+      
+          ${timeZone} ${date} ${created} 
+      </div>`
+  );
 });
 
 const PORT = 3003;
